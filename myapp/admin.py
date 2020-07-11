e547381890393f0aa7f91ca9234f4a18c653c070
Ryan Serkouh <hserkouh@sfu.ca>
2020-07-10 17:01:02 -0700

    Added admin searchbar on admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -3,4 +3,11 @@ from django.contrib import admin
 from .models import Notion
 # Register your models here.
 
-admin.site.register(Notion)
+class NotionAdmin(admin.ModelAdmin):
+    list_display = ['__str__','user']
+    search_fields = ['user__username','user__email']
+    class Meta:
+        model = Notion
+
+
+admin.site.register(Notion,NotionAdmin)
