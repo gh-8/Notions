32e7c400da8ead4a03c2ef59d9bbbf6a57e7dcc4
Ryan Serkouh <hserkouh@sfu.ca>
2020-07-13 19:35:03 -0700

    QA: Added test(create user -create notion)

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
+from .models import Notion
 # Create your tests here.
+User = get_user_model()
+
+class NotionTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',password='somepw')
+
+    def test_notion_created(self):
+        notion_obj = Notion.objects.create(content="My Notion", user=self.user)
+        self.assertEqual(notion_obj.id,1)
+        self.assertEqual(notion_obj.user, self.user)
+
+
+
+        
